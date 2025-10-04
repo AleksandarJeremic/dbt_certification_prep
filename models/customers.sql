d7cb5463958f0af01b0537d249c1590bf82b494a
Aleksandar Jeremić <ajeremic@parkoursc.com>
2025-10-04 21:05:29 +0200

    init

--- models/customers.sql
+++ models/customers.sql
@@ -4,8 +4,7 @@ with customers as (
         id as customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
+    from {{ source('tutorial', 'customers') }}
 
 ),
 
@@ -16,8 +15,7 @@ orders as (
         user_id as customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    from {{ source('tutorial', 'orders') }}
 
 ),
 
@@ -25,31 +23,25 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
     group by 1
 
 ),
 
-
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders
+    from customers c
+    left join customer_orders co using (customer_id)
 
 )
 
