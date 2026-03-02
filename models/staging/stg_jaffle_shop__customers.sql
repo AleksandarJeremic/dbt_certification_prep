50523c4976cb2530e990ba17e5d2dd5c444b7ba3
Aleksandar Jeremić <ajeremic@parkoursc.com>
2026-03-02 11:55:00 +0100

    surrogate key+new packages

--- models/staging/stg_jaffle_shop__customers.sql
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -1,5 +1,6 @@
 select
         id as customer_id,
         first_name,
-        last_name
+        last_name,
+        {{ dbt_utils.generate_surrogate_key(['id','first_name','last_name']) }} as customer_surrogate_key,
 from {{ source('tutorial', 'customers') }}
